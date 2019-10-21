7d285099e5aefc4df6ec9fcffce0ed7f0d306222
craigreedwilliams <craig.williams001@student.swic.edu>
2019-10-20 22:14:15 -0500

    First commit.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,22 @@
-// Enter a description of this program here.
+// project 1
 
 /**
- * Write some pseudo code steps here.
- * Use those steps as your `commit` messages (and delete them from here as you complete a step.)
+ * This app will estimate the length of a trip.
  */
+
+const tripCalculator = {
+  getTripLength(a, b) {
+    return a / b;
+  }
+};
+
+const form = document.querySelector("form");
+const num1 = document.querySelector("#num-1");
+const num2 = document.querySelector("num-2");
+const select = document.querySelector("select");
+const output = document.querySelector("output");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  output.textContent = tripCalculator[select.value](num1.value, num2.value);
+});
